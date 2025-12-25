dde97af585ba81781c5e9598d37a9e8149f40d0f
Sosangkar saikia <saikiasosangkar11@gmail.com>
2025-12-25 14:49:51 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#creating directory
+mkdir -p src
+mkdir -p research
+
+#creatingg files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requriments.txt
+ 
+
+ echo "Directory and files creatd successfully!."
